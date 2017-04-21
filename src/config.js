547d78c453f9956c1789df751615f5c0352f2de0
Inès BENSALAH <ibensalah@pactenovation.fr>
2017-04-21 09:28:16 +0200

    config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-process.env.PORT = '5000'
-process.env.REQUEST_TOKEN = ''
+process.env.REQUEST_TOKEN = 'ef04d664f0477ad4852e69dfdad1b47b'
 process.env.LANGUAGE = 'fr'
-process.env.GITHUB_TOKEN = ' b90746a24f9047b77dc9938c2202055b739398b8  '
-process.env.GITHUB_USERNAME = 'InesBensalah'
+process.env.PORT = '5000'
